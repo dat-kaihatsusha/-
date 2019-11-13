77bc8a7fd93eff17234123fa0e080a915f123384
dat-kaihatsusha <57696775+dat-kaihatsusha@users.noreply.github.com>
2019-11-13 11:24:49 +0700

    Add files via upload

---
+++ 01.php
@@ -0,0 +1,7 @@
+Bài toán: In dữ liệu sinh viên có quê là HN trong câu truy vấn bằng PHP
+<?php 
+require 'connect.php';
+$sql = "SELECT sinhvien.* FROM sinhvien WHERE sinhvien.NoiSinh = 'Hà Nội'";
+// Thực thi lệnh
+
+ ?>
